f539c8039f6bf1ec234a8aa8ea44fbe9543e986e
Liogorani <Jelie0375@gmail.com>
2023-02-16 02:51:13 +0100

    hello world

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function member(id, name, grade) {
+    this.id       = id;
+    this.name     = name;
+    this.grade    = grade;
+    this.toString = function() {
+      return "ID: "+this.id+", Name: "+this.name+", Grade: "+this.grade;
+    }
+  }
+   
+  function team() {
+    this.members  = new Array();
+    this.add      = function(member) {
+      var index           = this.members.length;
+      this.members[index] = member;
+      return index;
+    }
+    this.toString = function() {
+      return this.members.join("\n");
+    }
+  }
+   
+  var myTeam = new team();
+   
+  myTeam.add(new member(42, "Alice", "ICT C"));
+  myTeam.add(new member(43, "Abdelhamid", "ICT C"));
+  myTeam.add(new member(44, "Baya", "ICT C"));
+  myTeam.add(new member(45, "Firmus", "ICT B"));
+  myTeam.add(new member(46, "Jean", "ICT C"));
+  myTeam.add(new member(47, "Badria", "ICT A"));
+  myTeam.add(new member(48, "Lalla", "ICT C"));
+  myTeam.add(new member(49, "Souleymane", "ICT B"));
+   
+  document.writeln(myTeam);
